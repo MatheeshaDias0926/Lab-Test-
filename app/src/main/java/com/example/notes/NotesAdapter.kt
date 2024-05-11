7f1d5079bc186b66d3e3bc50d304a1c08f062559
MatheeshaDias0926 <155762517+MatheeshaDias0926@users.noreply.github.com>
2024-05-11 16:39:52 +0530

    added new layout for displaying added notes

---
+++ app/src/main/java/com/example/notes/NotesAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.notes
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter(private var notes: List<Note>,context:Context):RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
+
+    class NoteViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+        val titleTextView : TextView = itemView.findViewById(R.id.titleTextView)
+        val contentTextView : TextView = itemView.findViewById(R.id.contentTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
