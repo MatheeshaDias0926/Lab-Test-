507b6ba74f70e4d4ced2551a1a596e6747dc27a5
MatheeshaDias0926 <155762517+MatheeshaDias0926@users.noreply.github.com>
2024-05-11 20:20:44 +0530

    finalized project by implemeting delete function

--- app/src/main/java/com/example/notes/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/notes/NotesDatabaseHelper.kt
@@ -83,6 +83,15 @@ class NotesDatabaseHelper(context: Context):SQLiteOpenHelper(context, DATABASE_N
         return Note(id,title,content)
 
     }
+    fun deleteNote(noteId: Int){
+        val db = writableDatabase
+        val whereClause ="$COLUMN_ID = ?"
+        val whereArgs =  arrayOf(noteId.toString())
+        db.delete(TABLE_NAME,whereClause,whereArgs)
+        db.close()
+
+    }
+
 
 
 }
